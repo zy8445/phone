14ad1a690edb4cdcaf1341a30d1d2fea8ecde994
zhangying <675461534@qq.com>
2019-04-08 22:30:34 +0800

    完善

---
+++ total.js
@@ -0,0 +1,8 @@
+$(function(){
+	var dingwei = $(".dingwei");
+	if(dingwei.width<640px){
+		dingwei.css({"position","absolute":"left","15%"});
+//	}else{
+//		dingwei.css("position","absolute":"left","33.4%");
+	}
+})
